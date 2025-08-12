4a3e3ef749a04a95ab8451d48d1f5ec3b0dc9679
x9nst <97040475+x9nst@users.noreply.github.com>
2025-08-13 05:54:11 +0600

    feat: enhance navbar animations

--- var/www/frontend-next/tailwind.config.js
+++ var/www/frontend-next/tailwind.config.js
@@ -15,6 +15,23 @@ module.exports = {
           800: '#1f2937',
           900: '#111827'
         }
+      },
+      keyframes: {
+        bump: {
+          '0%': { transform: 'scale(1)' },
+          '10%': { transform: 'scale(1.1)' },
+          '30%': { transform: 'scale(1.15)' },
+          '50%': { transform: 'scale(1.1)' },
+          '100%': { transform: 'scale(1)' }
+        },
+        'micro-bounce': {
+          '0%,100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-2px)' }
+        }
+      },
+      animation: {
+        bump: 'bump 0.3s',
+        'micro-bounce': 'micro-bounce 0.3s'
       }
     }
   },
