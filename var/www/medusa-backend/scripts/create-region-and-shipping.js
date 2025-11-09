f48493b5600bd3814e23a44a83c345e695e2de8e
x9nst <iqrammahmud00@gmail.com>
2025-11-09 14:53:17 +0600

    refactor: simplify region and shipping option creation logic

--- var/www/medusa-backend/scripts/create-region-and-shipping.js
+++ var/www/medusa-backend/scripts/create-region-and-shipping.js
@@ -1,60 +1,27 @@
-#!/usr/bin/env node
-const Medusa = require('@medusajs/medusa').default || require('@medusajs/medusa')
-const path = require('path')
+const Medusa = require('@medusajs/medusa')
+const config = require('../medusa-config')
 
-const backendRoot = path.join(__dirname, '..')
-const config = require(path.join(backendRoot, 'medusa-config'))
+const medusa = new Medusa({ projectConfig: config.projectConfig })
 
-const medusa = new Medusa({
-  projectConfig: config.projectConfig,
-  plugins: config.plugins,
-  modules: config.modules,
-  featureFlags: config.featureFlags,
-})
-
-const createRegion = async () => {
-  const existing = await medusa.admin.regions.list()
-  if (existing.regions && existing.regions.length) {
-    const head = existing.regions[0]
-    console.log('Using existing region', head.id)
-    return head.id
-  }
+async function main() {
   const region = await medusa.admin.regions.create({
     name: 'Bangladesh',
     currency_code: 'bdt',
     countries: ['BD'],
   })
-  console.log('Created region', region.id)
-  return region.id
-}
+  console.log('region id:', region.id)
 
-const createShippingOption = async (regionId) => {
-  const options = await medusa.admin.shippingOptions.list({ region_id: regionId })
-  const existing = options.shipping_options?.find((opt) => !opt.is_return)
-  if (existing) {
-    console.log('Using existing shipping option', existing.id)
-    return existing.id
-  }
   const shipping = await medusa.admin.shippingOptions.create({
     name: 'Standard',
-    region_id: regionId,
+    region_id: region.id,
     provider_id: 'manual',
     amount: 0,
   })
-  console.log('Created shipping option', shipping.id)
-  return shipping.id
+  console.log('shipping option id:', shipping.id)
+  process.exit(0)
 }
 
-const main = async () => {
-  try {
-    const regionId = await createRegion()
-    const shippingId = await createShippingOption(regionId)
-    console.log('Success:', { regionId, shippingId })
-    process.exit(0)
-  } catch (error) {
-    console.error('Failed to create entries', error)
-    process.exit(1)
-  }
-}
-
-main()
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
